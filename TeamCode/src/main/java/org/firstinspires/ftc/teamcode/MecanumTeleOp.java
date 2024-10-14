1b4889a07757957115b67ccd1e3219d15ec4d0cd
iluvboba0121 <diana.wj.chen7@gmail.com>
2024-10-13 20:49:19 -0700

    add DcMotorPowerModifierAdv()

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
@@ -21,9 +21,9 @@ public class MecanumTeleOp extends LinearOpMode {
         // reverse the left side instead.
         // See the note about this earlier on this page.
         backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontLeftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
         frontRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
-        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
 
         waitForStart();
 
@@ -31,7 +31,7 @@ public class MecanumTeleOp extends LinearOpMode {
 
         while (opModeIsActive()) {
             double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
-            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+            double x = gamepad1.left_stick_x; // Counteract imperfect strafing
             double rx = gamepad1.right_stick_x;
 
             // Denominator is the largest motor power (absolute value) or 1
@@ -43,17 +43,68 @@ public class MecanumTeleOp extends LinearOpMode {
             double frontRightPower = (y - x - rx) / denominator;
             double backRightPower = (y + x - rx) / denominator;
 
-            double speedReduction = 1;
+//            double speedReduction = 1;
+
 
             //frontLeftPower=Math.pow(Math.tanh(frontLeftPower)/Math.tanh(1),3);
-            frontLeftMotor.setPower(frontLeftPower*speedReduction);
+//            frontLeftMotor.setPower(frontLeftPower*speedReduction);
             //backLeftPower=Math.pow(Math.tanh(backLeftPower)/Math.tanh(1),3);
-            backLeftMotor.setPower(backLeftPower*speedReduction);
+//            backLeftMotor.setPower(backLeftPower*speedReduction);
             //frontRightPower=Math.pow(Math.tanh(frontRightPower)/Math.tanh(1),3);
-            frontRightMotor.setPower(frontRightPower*speedReduction);
+//            frontRightMotor.setPower(frontRightPower*speedReduction);
             //backRightPower=Math.pow(Math.tanh(backRightPower)/Math.tanh(1),3);
-            backRightMotor.setPower(backRightPower*speedReduction);
+//            backRightMotor.setPower(backRightPower*speedReduction);
+/*
+            double frontLeftPower_mod = Math.pow(Math.tanh(frontLeftPower)/Math.tanh(1),3);
+            double backLeftPower_mod = Math.pow(Math.tanh(backLeftPower)/Math.tanh(1),3);
+            double frontRightPower_mod = Math.pow(Math.tanh(frontRightPower)/Math.tanh(1),3);
+            double backRightPower_mod = Math.pow(Math.tanh(backRightPower)/Math.tanh(1),3);
 
+            // modify power by using DcMotorPowerModifier()
+            double frontLeftPower_mod = DcMotorPowerModifier(frontLeftPower);
+            double backLeftPower_mod = DcMotorPowerModifier(backLeftPower);
+            double frontRightPower_mod = DcMotorPowerModifier(frontRightPower);
+            double backRightPower_mod = DcMotorPowerModifier(backRightPower);
+*/
+            // modify power by using DcMotorPowerModifierAdv()
+            int eqVer = 1;
+            double frontLeftPower_mod = DcMotorPowerModifierAdv(frontLeftPower, eqVer);
+            double backLeftPower_mod = DcMotorPowerModifierAdv(backLeftPower, eqVer);
+            double frontRightPower_mod = DcMotorPowerModifierAdv(frontRightPower, eqVer);
+            double backRightPower_mod = DcMotorPowerModifierAdv(backRightPower, eqVer);
+
+
+            frontLeftMotor.setPower(frontLeftPower_mod);
+            backLeftMotor.setPower(backLeftPower_mod);
+            frontRightMotor.setPower(frontRightPower_mod);
+            backRightMotor.setPower(backRightPower_mod);
+
+            telemetry.addData("frontLeftPower_mod: ", frontLeftPower);
+            telemetry.addData("frontLeftPower_mod: ", backLeftPower);
+            telemetry.addData("frontLeftPower_mod: ", frontRightPower);
+            telemetry.addData("frontLeftPower_mod: ", backRightPower);
+            telemetry.addData("frontLeftPower_mod: ", frontLeftPower_mod);
+            telemetry.addData("frontLeftPower_mod: ", backLeftPower_mod);
+            telemetry.addData("frontLeftPower_mod: ", frontRightPower_mod);
+            telemetry.addData("frontLeftPower_mod: ", backRightPower_mod);
+
+            telemetry.update();
+        }
+    }
+
+    private double DcMotorPowerModifier(double Power) {
+        return Math.pow(Math.tanh(Power)/Math.tanh(1),3);
+    }
+
+    private double DcMotorPowerModifierAdv(double Power, int eqVer) {
+        if (eqVer == 1) {
+            return Math.pow(Math.tanh(Power)/Math.tanh(1),3);
+        } else if (eqVer == 2) {
+            return Math.pow(Power, 3);
+        } else if (eqVer == 3) {
+            return Math.pow(Power, 5);
+        } else {
+            return Power;
         }
     }
 }
