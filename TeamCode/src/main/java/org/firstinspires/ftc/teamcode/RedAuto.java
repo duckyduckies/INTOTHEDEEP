f6a42e2eeb6107c091d951d293077b3e67d82226
IdiotsandInnovation <bookpediaentertainment@gmail.com>
2024-11-21 11:28:44 -0800

    Added RedAuto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedAuto.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@Autonomous(name="RedAuto", group="Linear Opmode")
+public class RedAuto extends LinearOpMode {
+
+    private DcMotor frontLeftMotor = null;
+    private DcMotor frontRightMotor = null;
+    private DcMotor backLeftMotor = null;
+    private DcMotor backRightMotor = null;
+
+    @Override
+    public void runOpMode() {
+        // Initialize the hardware variables
+        frontLeftMotor = hardwareMap.get(DcMotor.class, "frontLeftMotor");
+        frontRightMotor = hardwareMap.get(DcMotor.class, "frontRightMotor");
+        backLeftMotor = hardwareMap.get(DcMotor.class, "backLeftMotor");
+        backRightMotor = hardwareMap.get(DcMotor.class, "backRightMotor");
+
+        // Set the direction of the motors
+        backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontLeftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        frontRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        backRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        // Autonomous actions
+        driveForward(1.0, 2000); // Drive forward with full power for 2 seconds
+        strafeRight(0.5, 1000);  //
+    }
+}
